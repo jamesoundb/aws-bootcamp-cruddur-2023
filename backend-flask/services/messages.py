6cce88e369cc03cb4f4ed952ce09366f54d91620
James Wurbel <jameswurbel@gmail.com>
2023-02-20 20:33:51 +0000

    Update services/messages.py

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -13,7 +13,7 @@ class Messages:
         'uuid': '4e81c06a-db0f-4281-b4cc-98208537772a' ,
         'display_name': 'Andrew Brown',
         'handle':  'andrewbrown',
-        'message': 'Cloud is better than on prem!',
+        'message': 'Cloud is fun!',
         'created_at': now.isoformat()
       },
       {
