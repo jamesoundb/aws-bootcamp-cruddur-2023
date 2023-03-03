d816e364c418abd1bb89eeed84a3f4eb901ee10e
James Wurbel <jameswurbel@gmail.com>
2023-03-03 00:39:01 +0000

    Update to xray decorators for endpoints

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -154,6 +154,7 @@ def data_notifications():
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
+@xray_recorder.capture('user_activities')
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
