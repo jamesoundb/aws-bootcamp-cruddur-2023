04cd58f89164b96cfbbba6b39f39549dade2dd64
James Wurbel <jameswurbel@gmail.com>
2023-03-06 21:32:34 +0000

    Add envar values to compose and added Signin error

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -14,21 +14,20 @@ export default function SigninPage() {
 
   const onsubmit = async (event) => {
     setErrors('')
+    // console.log('onsubmit')
     event.preventDefault();
-    try {
-      Auth.signIn(email, password)
-        .then(user => {
-          localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
-          window.location.href = "/"
-        })
-        .catch(err => { console.log('Error!', err) });
-    } catch (error) {
+    Auth.signIn(email, password)
+    .then(user => {
+      localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+      window.location.href = "/"
+    })
+    .catch(error => { 
       if (error.code == 'UserNotConfirmedException') {
-        window.location.href = "/confirm"
+      window.location.href = "/confirm"
       }
-      setErrors(error.message)
-    }
-    return false
+      setErrors(error.message)  
+    });
+   return false
   }
 
   const email_onchange = (event) => {
