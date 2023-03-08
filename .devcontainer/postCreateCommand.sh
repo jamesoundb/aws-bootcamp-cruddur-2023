537bfebdb434971a1b1b816b624c28840fcdb688
James Wurbel <jameswurbel@gmail.com>
2023-03-08 15:37:26 +0000

    Updated devcontainer.json

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# npm install frontend
+cd /workspaces/aws-bootcamp-cruddur-2023/frontend-react-js && npm update -g && npm i
+
+cd /workspaces/aws-bootcamp-cruddur-2023/backend-flask && pip3 install -r requirements.txt
+
+# Postgresql
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main' |sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt update -y
+sudo apt install -y postgresql-client-13 libpq-dev
