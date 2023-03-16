67384746ff3ee0b69ec64b3876b0a22ee8ae591b
James Wurbel <jameswurbel@gmail.com>
2023-03-16 22:28:35 +0000

    Added RDS Update SG Rule

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,4 +10,7 @@ cd /workspaces/aws-bootcamp-cruddur-2023/backend-flask && pip3 install -r requir
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list';
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -;
 sudo apt-get update -y;
-sudo apt install -y postgresql-client-13 libpq-dev
+sudo apt install -y postgresql-client-13 libpq-dev;
+
+# RDS Update SG Rule
+./workspaces/aws-bootcamp-cruddur-2023/backend-flask/bin/rds-update-sg-rule-codespaces
