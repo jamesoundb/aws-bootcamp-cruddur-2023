2cfc54b48a7055c5d16e09ccb274a00486e9ec6f
James Wurbel <jameswurbel@gmail.com>
2023-03-29 00:54:33 +0000

    Created ddb utility scripts

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -34,16 +34,16 @@ class Db:
     for k, v in params.items():
       print(k, ":", v)
 
-  def print_sql(self, title, sql):
+  def print_sql(self, title, sql, params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f"{cyan} SQL STATEMENT>>[{title}]>>>>>>>>{no_color}")
-    print(sql)
+    print(sql, params)
   
   # Check to make sure that RETURNING is in all uppercase
   # When we want to commit data such as an insert with returning id
   def query_commit(self, sql, params={}):
-    self.print_sql("commit with returning id", sql)
+    self.print_sql("commit with returning id", sql, params)
     print(sql + "\n")
     
     pattern = r"\bRETURNING\b"
@@ -61,9 +61,19 @@ class Db:
     except Exception as err:
       self.print_sql_err(err)  
 
+   # when we want to return a a single value
+  def query_value(self,sql,params={}):
+    self.print_sql('value', sql, params)
+
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql, params)
+        json = cur.fetchone()
+        return json[0]
+
   # When we want to return an array of json objects
   def query_array_json(self, sql, params={}):
-    self.print_sql("array", sql)
+    self.print_sql("array", sql, params)
    
     wrap_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -77,7 +87,7 @@ class Db:
   
    # When we want to return a json object
   def query_object_json(self, sql, params={}):
-    self.print_sql("json", sql)
+    self.print_sql("json", sql, params)
     self.print_params(params)
 
     wrap_sql = self.query_wrap_object(sql)
