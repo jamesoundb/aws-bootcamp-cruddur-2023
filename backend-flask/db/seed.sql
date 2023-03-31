12c3abc5ba18e8073fb3d69cf84b06a0669b63e8
James Wurbel <jameswurbel@gmail.com>
2023-03-31 01:10:21 +0000

    Update users and create uuid from cognito user id

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'mock_1@mock.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko', 'mock_2@mock.com', 'MOCK');
+  ('Andrew Brown', 'andrewbrown', 'andrew@mock.com', '2104de31-c4ae-4a31-b396-93bec358a660'),
+  ('James Wurbel', 'jamesoundb' ,'jameswurbel.it@gmail.com', '7a310240-e888-4f0a-b9f5-7a434a8a471f'),
+  ('James Wurbel', 'jamesoundb II', 'jameswurbel@gmail.com', 'fb1a437a-1457-4dbc-982e-5d0be7cecd76');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
