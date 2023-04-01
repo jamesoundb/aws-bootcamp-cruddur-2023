d82a13df2c07aa8fc2b72fee3d9ccb81547a5d9b
James Wurbel <jameswurbel@gmail.com>
2023-04-01 22:07:23 +0000

    Updated seed data

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown', 'andrew@mock.com', '2104de31-c4ae-4a31-b396-93bec358a660'),
   ('James Wurbel', 'jamesoundb' ,'jameswurbel.it@gmail.com', '7a310240-e888-4f0a-b9f5-7a434a8a471f'),
+  ('Andrew Bayko', 'bayko', 'bayko@mock.com', '8vc48b2f-x548-8ejs-w24d-2ic9wiwhfna3'),
   ('James Wurbel', 'jamesoundb II', 'jameswurbel@gmail.com', 'fb1a437a-1457-4dbc-982e-5d0be7cecd76');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'jamesoundb' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
