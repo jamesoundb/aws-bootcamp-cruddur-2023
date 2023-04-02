e954d40cfb08483e278ded19176fb5ce5fc4c41f
James Wurbel <jameswurbel@gmail.com>
2023-04-02 22:59:51 +0000

    Updated seed data and added short.sql file

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('James Wurbel', 'jamesoundb' ,'jameswurbel.it@gmail.com', '7a310240-e888-4f0a-b9f5-7a434a8a471f'),
   ('Andrew Bayko', 'bayko', 'bayko@mock.com', '8vc48b2f-x548-8ejs-w24d-2ic9wiwhfna3'),
-  ('James Wurbel', 'jamesoundb II', 'jameswurbel@gmail.com', 'fb1a437a-1457-4dbc-982e-5d0be7cecd76');
+  ('Kylo Ren', 'Kylo', 'Kylo@sithlord.com', '666sithd-arks-ider-ules-2yclziwzfja5');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
