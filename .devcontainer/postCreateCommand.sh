b6e7d1e339bfb553eae28f5356d3ffe932403b58
James Wurbel <jameswurbel@gmail.com>
2023-04-11 17:31:29 -0700

    Update SG Rule script path and add session manager plugin

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -14,4 +14,7 @@ sudo apt install -y postgresql-client-13 libpq-dev;
 
 # RDS Update SG Rule
 export CODESPACES_IP="$(curl ifconfig.me)"
-cd /workspaces/aws-bootcamp-cruddur-2023 && bash ./backend-flask/bin/rds/update-sg-rule-codespaces
+cd /workspaces/aws-bootcamp-cruddur-2023 && bash ./bin/rds/update-sg-rule-codespaces
+
+# Install-session-manager-plugin
+"curl 'https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb' -o 'session-manager-plugin.deb' && sudo dpkg -i session-manager-plugin.deb"
