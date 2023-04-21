99ea4041775d1ddb9d25c13ef2b404860c86c28a
James Wurbel <jameswurbel@gmail.com>
2023-04-20 17:05:39 -0700

    Added generate envs

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -19,7 +19,10 @@ cd /workspaces/aws-bootcamp-cruddur-2023 && bash ./bin/rds/update-sg-rule-codesp
 
 # ALB Update SG Rule
 export LOCAL_IP=198.54.133.149
-cd /workspaces/aws-bootcamp-cruddur-2023 && bash ./bin/alb/update-sg-rule
+cd /workspaces/aws-bootcamp-cruddur-2023 && bash ./bin/alb/update-sg-rule;
 
 # Install-session-manager-plugin
-curl 'https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb' -o 'session-manager-plugin.deb' && sudo dpkg -i session-manager-plugin.deb
+curl 'https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb' -o 'session-manager-plugin.deb' && sudo dpkg -i session-manager-plugin.deb;
+
+#Generate Envs
+/workspaces/aws-bootcamp-cruddur-2023/bin/backend/generate-env-codespaces && /workspaces/aws-bootcamp-cruddur-2023/bin/frontend/generate-env-codespaces
